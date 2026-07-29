c3758b2d4195a4d742a78809999f90da486a8551
kavyansh <wadhwakavyansh2008@gmail.com>
2026-07-29 22:27:19 +0530

    file added

--- loader.py
+++ loader.py
@@ -7,10 +7,13 @@ data={'Name':['Alice','Bob','Charlie'],
 
 df=pd.DataFrame(data)
 
+new_row_loc = {'Name':'GF1','Age':20,'City':'city1'}
+df.loc[len(df.index)] = new_row_loc
+
 data_dir='data'
 os.makedirs(data_dir,exist_ok=True)
 
-file_path=os.path.join(data_dir,'sample_data,csv')
+file_path=os.path.join(data_dir,'sample_data.csv')
 
 df.to_csv(file_path,index=False)
 
